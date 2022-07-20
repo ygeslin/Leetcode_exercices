da1c2760a6ac6d3647a2f883e8350a0cf131371b
Yann GESLIN <ygeslin@e1r6p15.clusters.42paris.fr>
2022-07-20 10:55:04 +0200

    beautify comments

--- Array/MoveZeroes.js
+++ Array/MoveZeroes.js
@@ -1,44 +1,42 @@
-// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
-
-// Note that you must do this in-place without making a copy of the array.
-
+// * Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+// * Note that you must do this in-place without making a copy of the array.
 
 
-// Example 1:
 
-// Input: nums = [0,1,0,3,12]
-// Output: [1,3,12,0,0]
-// Example 2:
+// * Example 1:
+// * Input: nums = [0,1,0,3,12]
+// * Output: [1,3,12,0,0]
 
-// Input: nums = [0]
-// Output: [0]
-
-
-// Constraints:
-
-// 1 <= nums.length <= 104
-// -231 <= nums[i] <= 231 - 1
+// * Example 2:
+// * Input: nums = [0]
+// * Output: [0]
 
 
-// Follow up: Could you minimize the total number of operations done?
-//    Hide Hint #1
-// In-place means we should not be allocating any space for extra array. But we are allowed to modify the existing array. However, as a first step, try coming up with a solution that makes use of additional space. For this problem as well, first apply the idea discussed using an additional array and the in-place solution will pop up eventually.
-//    Hide Hint #2
-// A two-pointer approach could be helpful here. The idea would be to have one pointer for iterating the array and another pointer that just works on the non-zero elements of the array.
+// * Constraints:
+
+// * 1 <= nums.length <= 104
+// * -231 <= nums[i] <= 231 - 1
+
+
+// * Follow up: Could you minimize the total number of operations done?
+// *    Hide Hint #1
+// * In-place means we should not be allocating any space for extra array. But we are allowed to modify the existing array. However, as a first step, try coming up with a solution that makes use of additional space. For this problem as well, first apply the idea discussed using an additional array and the in-place solution will pop up eventually.
+// *    Hide Hint #2
+// * A two-pointer approach could be helpful here. The idea would be to have one pointer for iterating the array and another pointer that just works on the non-zero elements of the array.
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    //var pos = 0;
-    //for (var i = 0; i < nums.length; i++) {
-    //   if (nums[i] !== 0) {
-    //      nums[pos++] = nums[i];
+    // var pos = 0;
+    // for (var i = 0; i < nums.length; i++) {
+    //    if (nums[i] !== 0) {
+    //       nums[pos++] = nums[i];
+    //  }
     // }
-    //}
-    //for (i = pos; i < nums.length; i++) {
-    //   nums[i] = 0;
-    //}
+    // for (i = pos; i < nums.length; i++) {
+    //    nums[i] = 0;
+    // }
     let count = 0;
     for (let i = 0; i < nums.length; i++)
     {
--- Strings/ReverseString.js
+++ Strings/ReverseString.js
@@ -1,25 +1,25 @@
-// Write a function that reverses a string. The input string is given as an array of characters s.
+// * Write a function that reverses a string. The input string is given as an array of characters s.
 
-// You must do this by modifying the input array in-place with O(1) extra memory.
-
-
-
-// Example 1:
-
-// Input: s = ["h","e","l","l","o"]
-// Output: ["o","l","l","e","h"]
-// Example 2:
-
-// Input: s = ["H","a","n","n","a","h"]
-// Output: ["h","a","n","n","a","H"]
+// * You must do this by modifying the input array in-place with O(1) extra memory.
 
 
-// Constraints:
 
-// 1 <= s.length <= 105
-// s[i] is a printable ascii character.
-//    Hide Hint #1
-// The entire logic for reversing a string is based on using the opposite directional two-pointer approach!
+// * Example 1:
+
+// * Input: s = ["h","e","l","l","o"]
+// * Output: ["o","l","l","e","h"]
+// * Example 2:
+
+// * Input: s = ["H","a","n","n","a","h"]
+// * Output: ["h","a","n","n","a","H"]
+
+
+// * Constraints:
+
+// * 1 <= s.length <= 105
+// * s[i] is a printable ascii character.
+// *    Hide Hint #1
+// * The entire logic for reversing a string is based on using the opposite directional two-pointer approach!
 /**
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
@@ -27,15 +27,15 @@
 var reverseString = function(s) {
      let i = 0, j = s.length - 1;
   while (i < j) {
-    // ES6 destructuring assignment
+    // * ES6 destructuring assignment
     [s[i], s[j]] = [s[j], s[i]];
     i++;
     j--;
   }
-//    let start = 0;
-//    let end = s.length - 1
-//    for (; start < end;start++, end--)
-//    {
-//        [s[start], s[end]] = [s[end],s[start]]
- //   }
+// let start = 0;
+// let end = s.length - 1
+// for (; start < end;start++, end--)
+// {
+//     [s[start], s[end]] = [s[end],s[start]]
+//}
 };
